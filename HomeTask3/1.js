a244ea77f1be8f1458ebee2c4f8faae57275a19d
onlyvasya <onlyvasya@mail.ru>
2023-04-23 01:21:24 +0200

    исправления

--- HomeTask3/1.js
+++ HomeTask3/1.js
@@ -1,3 +1,3 @@
 const tempC = Number.parseFloat(prompt("Введите температура в цельсиях"));
-const tempF = 9 / 5 * tempC + 32;
-alert(`Цельсий: ${tempC}, Ференгейт: ${tempF}`)
+const tempF = (9 / 5) * tempC + 32;
+alert(`Цельсий: ${tempC}, Ференгейт: ${tempF.toFixed(1)}`);
